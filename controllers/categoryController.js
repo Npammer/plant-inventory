c2ef047b1da0b01f0e96849c32bb5ebc4c7c4d75
npammer <npammer@gmail.com>
2020-10-25 10:06:58 +0100

    backend done

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -65,17 +65,104 @@ exports.category_create_post = [
   },
 ];
 
-exports.category_delete_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category delete post");
+exports.category_delete_post = function (req, res, next) {
+
+  async.parallel({
+    category: function (callback) {
+      Category.findById(req.params.id).exec(callback)
+    },
+    plants: function (callback) {
+      Plant.find({
+        'category': req.params.id
+      }).exec(callback)
+    },
+  }, function (err, results) {
+    if (err) {
+      return next(err);
+    }
+    // Success
+    if (results.plants.length > 0) {
+      // book has books. Render in same way as for GET route.
+      res.render("category_detail", {
+        title: "Category Detail",
+        category: results.category,
+        plants: results.plants,
+        delete: true,
+      });
+      return;
+    } else {
+      // book has no books. Delete object and redirect to the list of books.
+      Category.findByIdAndRemove(req.params.id, function deleteCategory(err) {
+        if (err) {
+          return next(err);
+        }
+        // Success - go to book list
+        res.redirect('/categories')
+      })
+    }
+  });
 };
 
 exports.category_update_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category update get");
+  Category.findById(req.params.id).exec(function (err, category) {
+    if (err) {
+      return next(err);
+    }
+    //Successful, so render
+    res.render("category_form", {
+      title: "Update category",
+      category,
+    });
+  });
 };
 
-exports.category_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category update post");
-};
+exports.category_update_post = [
+  body("name", "Title must not be empty.")
+  .trim()
+  .isLength({
+    min: 1,
+  })
+  .escape(),
+  body("description").trim().escape(),
+  body("url").trim().escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Book object with escaped and trimmed data.
+    let category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+      url: req.body.url,
+      _id: req.params.id
+    });
+
+    if (!errors.isEmpty()) {
+      Category.findById(req.params.id).exec(function (err, category) {
+        if (err) {
+          return next(err);
+        }
+        //Successful, so render
+        res.render("category_form", {
+          title: "Update category",
+          category,
+        });
+      });
+      return;
+    } else {
+      // Data from form is valid. Save book.
+      Category.findByIdAndUpdate(req.params.id, category, {}, function (err, thecategory) {
+        if (err) {
+          return next(err);
+        }
+        //successful - redirect to new book record.
+        res.redirect(thecategory.link);
+      });
+    }
+  },
+];
 
 exports.category_detail = function (req, res, next) {
   async.parallel({
--- routes/catalog.js
+++ routes/catalog.js
@@ -1,43 +1,43 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // Controllers
-const plant_controller = require('../controllers/plantController');
-const category_controller = require('../controllers/categoryController');
+const plant_controller = require("../controllers/plantController");
+const category_controller = require("../controllers/categoryController");
 
 // INDEX ROUTE
-router.get('/', plant_controller.index);
+router.get("/", plant_controller.index);
 
 // PLANT ROUTES
 
-router.get('/plants', plant_controller.plant_list);
+router.get("/plants", plant_controller.plant_list);
 
-router.get('/plant/create', plant_controller.plant_create_get);
+router.get("/plant/create", plant_controller.plant_create_get);
 
-router.post('/plant/create', plant_controller.plant_create_post);
+router.post("/plant/create", plant_controller.plant_create_post);
 
-router.get('/plant/:id/delete', plant_controller.plant_delete_post);
+router.get("/plant/:id/delete", plant_controller.plant_delete_post);
 
-router.get('/plant/:id/update', plant_controller.plant_update_get);
+router.get("/plant/:id/update", plant_controller.plant_update_get);
 
-router.post('/plant/:id/update', plant_controller.plant_update_post);
+router.post("/plant/:id/update", plant_controller.plant_update_post);
 
-router.get('/plant/:id', plant_controller.plant_detail);
+router.get("/plant/:id", plant_controller.plant_detail);
 
 // CATALOG ROUTES
 
-router.get('/categories', category_controller.category_list);
+router.get("/categories", category_controller.category_list);
 
-router.get('/category/create', category_controller.category_create_get);
+router.get("/category/create", category_controller.category_create_get);
 
-router.post('/category/create', category_controller.category_create_post);
+router.post("/category/create", category_controller.category_create_post);
 
-router.post('/category/:id/delete', category_controller.category_delete_post);
+router.get("/category/:id/delete", category_controller.category_delete_post);
 
-router.get('/category/:id/update', category_controller.category_update_get);
+router.get("/category/:id/update", category_controller.category_update_get);
 
-router.post('/category/:id/update', category_controller.category_update_post);
+router.post("/category/:id/update", category_controller.category_update_post);
 
-router.get('/category/:id', category_controller.category_detail);
+router.get("/category/:id", category_controller.category_detail);
 
 module.exports = router;
