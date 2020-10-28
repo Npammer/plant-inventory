283fee193ce32e65b755e0bb474f9ca813c478c9
npammer <npammer@gmail.com>
2020-10-28 17:55:35 +0100

    Revert "added js to calculate hv"
    This reverts commit 5aaf599135b4807631b22a9bbcc1364fa03e6a04.

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,7 +1,2 @@
-// Calculate vh
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// Burger animation class
 const menuBtn = document.querySelector('.menu-btn');
 menuBtn.addEventListener('click', () => menuBtn.classList.toggle('open'));
