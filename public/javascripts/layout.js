ee5abf612fcfc17822c80a60a39fcd7a4ca6f85c
npammer <npammer@gmail.com>
2020-10-28 17:55:31 +0100

    Revert "changed style from vh to %"
    This reverts commit cfac4aa8f0a72d9a455620e6e8238315a256a9d8.

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,3 +1,7 @@
+// Calculate vh
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 // Burger animation class
 const menuBtn = document.querySelector('.menu-btn');
 menuBtn.addEventListener('click', () => menuBtn.classList.toggle('open'));
