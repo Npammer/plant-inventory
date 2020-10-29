406e746724a6882636399f47cd2fbc6794c0691a
npammer <npammer@gmail.com>
2020-10-29 12:27:12 +0100

    vh js calc working

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,2 +1,8 @@
+// Calculate vh
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// Burger menu add class
 const menuBtn = document.querySelector('.menu-btn');
 menuBtn.addEventListener('click', () => menuBtn.classList.toggle('open'));
