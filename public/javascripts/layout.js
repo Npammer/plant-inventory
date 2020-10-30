7dd5daf1452f919965363aa4997a406c1ee61fa7
npammer <npammer@gmail.com>
2020-10-30 10:56:17 +0100

    x scroll nav fixed

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -3,17 +3,17 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-// Burger menu add class and adnimation
 
+// Burger menu add class and adnimation
 const navAnimation = gsap.from(".nav", {
-  duration: 1,
-  x: window.innerWidth,
-  ease: "sine",
-  reversed: true,
+    duration: 1,
+    x: window.innerWidth,
+    ease: "sine",
+    reversed: true,
 });
 
 const menuBtn = document.querySelector(".menu-btn");
 menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
-  navAnimation.reversed() ? navAnimation.play() : navAnimation.reverse();
+    menuBtn.classList.toggle("open");
+    navAnimation.reversed() ? navAnimation.play() : navAnimation.reverse();
 });
